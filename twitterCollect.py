c48b1d38600c8b392534b1dae025b6745693198a
Yu Xiao <troyxiaoyu@gmail.com>
2018-05-23 17:12:03 -0400

    twitter location

--- twitterCollect.py
+++ twitterCollect.py
@@ -4,6 +4,7 @@ from tweepy import Stream
 from tweepy import StreamListener 
 from tweepy import OAuthHandler
 import json
+import time
 
 # Twitter API keys
 consumer_key = 'PSstLBpnp1oEgCtJQGFzSoXs0'
@@ -11,23 +12,37 @@ consumer_secret = 'f5v36gPN2u0cuBVujLqa2NBH3b5uJRRVTaGcGGtn1tEcM377vp'
 access_token = '998641782194933760-YCNSkx8udzUA5Yx5DLO3l2w4FjQfaVR'
 access_secret = 'PsHZQL76UQzwxsw9HnNFSd7U2c6gIcD5odWvO1ihjgMc7'
 
-# Authenticate
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
- 
 api = tweepy.API(auth)
-# Store stream twitters to the file
-file = open('streamTwitter', 'a')
 
-# This listener will print out all Tweets it receives
-class PrintListener(tweepy.StreamListener):
-    # Read data and store to file 
-    def on_data(self, data):
-        # Decode the JSON data
-        tweet = json.loads(data)
-        file.write(str(tweet))
-        # Print out the Tweet
-        print('@%s: %s' % (tweet['user']['screen_name'], tweet['text'].encode('ascii', 'ignore')))
+class MyStreamListener(tweepy.StreamListener):
+    
+    def __init__(self, time_limit=60):
+        self.start_time = time.time()
+        self.limit = time_limit
+        self.saveFile = open('streamTwitter.json', 'a')
+        super(MyStreamListener, self).__init__()
+
+    def on_status(self, status):
+        if (time.time() - self.start_time) < self.limit:
+            # filter twitter with geo info
+            if status.geo is not None:
+                self.saveFile.write(status.text)
+                self.saveFile.write('\n')
+
+                # print coordinates to screen
+                dictCoords = status.geo
+                listCoords = dictCoords['coordinates']
+                latitude = listCoords[0]
+                longitude = listCoords[1]
+                print(str(listCoords[0]) + "," + str(listCoords[1]))
+                print(status.place)
+
+                return True
+        else:
+            self.saveFile.close()
+            return False
 
     # Handle error
     def on_error(self, status_code):
@@ -38,26 +53,14 @@ class PrintListener(tweepy.StreamListener):
         print >> sys.stderr, 'Timeout...'
         return True # Don't kill the stream
 
-# Call self defined 
-def start_stream():
-    count = 0
-    while True and count<5:
-        try:
-            sapi = tweepy.streaming.Stream(auth, PrintListener(api))
-            sapi.sample()
-            count=count+1
-            # If want to filter by a certain word such as 'python', use the line below
-            # stream.filter(track=['python'])
-        except: 
-            continue
 
 if __name__ == '__main__':
-    # Show system message
-    print('Below are captured twitter ==>')
-
-    # Connect the stream to our listener
-    listener = PrintListener()
-    stream = tweepy.Stream(auth, listener)
+    # set the length of time to collect data
+    time_limit = 600
+    print('Collecting twitters...Wait time is '+ str(time_limit)+ ' seconds.'+'\n')
 
     # Collect data
-    start_stream()
+    myStream = tweepy.Stream(auth=api.auth, listener=MyStreamListener(time_limit))
+    myStream.sample()
+
+    print('Done.')
